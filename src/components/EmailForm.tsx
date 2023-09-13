9d62e9c6d8e2f983563973a9f8d037b916962e6d
Jerry Zhou <46613983+jerryzhou196@users.noreply.github.com>
2023-09-12 21:37:08 -0400

    prompt engineering 😎

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -25,7 +25,7 @@ const EmailForm: React.FC = () => {
           {
             role: "system",
             content:
-              'Your responses are going to be converted into a report that reflects how well the following emails comply with Interac\'s Compliance Policy. As a result, all your responses must be formatted as a JSON object. This JSON object should have an overall score in terms of percentage that reflects how well the email follows under the "score" property. In addition, it must have an array called quotes which contains key-value pairs - the key should be called "quote" and should be directly phrasing certain sentences within the email that violate any policies. The value should be called "comment" and you will write a short comment about why the quote violates any policies.',
+              'Your responses are going to be converted into a report that reflects how well the following emails comply with Interac\'s Compliance Policy. As a result, all your responses must be formatted as a JSON object. This JSON object should have an overall score in terms of percentage that reflects how well the email follows under the "score" property. In addition, it must have an array called quotes which contains key-value pairs - the key should be called "quote" and should be directly phrasing certain sentences within the email that violate any policies. The value should be called "comment" and you will write a short comment about why the quote violates any policies. The higher the score, the worst. You must put some quotes.',
           },
           {
             role: "system",

9ec11fe41c12ada0e7ed488fb571d817a4763623
Jerry Zhou <46613983+jerryzhou196@users.noreply.github.com>
2023-09-12 20:27:05 -0400

    added working API call

--- src/components/AnalyzeCompliace.tsx
+++ src/components/AnalyzeCompliace.tsx
@@ -6,7 +6,43 @@ interface AnalyzeComplianceProps {
 }
 
 const AnalyzeCompliance: React.FC<AnalyzeComplianceProps> = ({ isEmpty }) => {
-  const handleClick = () => {};
+  const handleClick = () => {
+    async function fetchCompletion() {
+      const url = "https://api.openai.com/v1/chat/completions";
+      const token = localStorage.getItem("token");
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      };
+      const body = JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: "You are a helpful assistant.",
+          },
+          {
+            role: "user",
+            content: "Hello!",
+          },
+        ],
+      });
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: headers,
+        body: body,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+      } else {
+        console.error("Failed to fetch:", response.status, response.statusText);
+      }
+    }
+    fetchCompletion();
+  };
 
   return (
     <Fragment>
